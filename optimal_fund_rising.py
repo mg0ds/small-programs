c003dfaabd187a619435c04d7ee7f91205ed49a9
mg0ds <53785137+mg0ds@users.noreply.github.com>
2022-06-17 21:51:28 +0200

    Update optimal_fund_rising.py

--- optimal_fund_rising.py
+++ optimal_fund_rising.py
@@ -1,3 +1,27 @@
+"""
+
+Here is another classic algorithm problem wrapped in a Bite. Enjoy!
+
+A mayor in a remote mountain village faced an emergency and he needs your help. He just learned a poor boy is very sick, and will need to go to the city hospital immediately.
+
+He only has one night to collect the contribution from the villagers, so that he can escort the boy and his family to the city next morning. Luckily, he has kept a 'secret list' of each family's "financial status + willingness to help" in his file over the years.
+
+So the challenge is how to find the starting family in the geographicly sparse mountain area, making sure he can collect maximum funds.
+
+The goal is to help the mayor to collect the maximum contribution and determine which families to start (and end). These are the key elements of this mission:
+
+To find the families in the closest neighborhood in the village with best possible donations that he can collect. You can think of this closest neighborhood as - "contiguous families block" in the "list", without circulating back. Since time is essence in this matter, he wants to get the funds quickly and prepare for next day's trip.
+
+Let's make some concrete examples to illustrate these points:
+
+Input: the list represents that each family's 'capacity' to contribute in $dollars_amount: it could be one of these values: positive, negative, or zero. For example: [0, 1, 2, 4, -3, -3]
+Note - the mountain village could have hundreds of families scattered around in wide area, so he would just go in one-direction. Therefore he prefers one-way trips (not circular!) - one pass. (this is known in CS as O(n) time requirement)
+Output: 3 items he needs to know - the maximum contribution (amount), startingfamily, endingfamily (eg. by its index). See below examples.
+Bonus - it will be great if you can help him find out the possibility of positive donations - e.g. there is at least one family can contribute before he starts the night trip. If the "donations" are negative - meaning all families are poor - then you could give him a warning message: "Mission impossible. No one can contribute.", exiting the program with (0, 0, 0) - see example 4 below.
+
+
+"""
+
 community = [3, 2, 6,  4, 7,  5, -8, -9, 3,  8,  4, -12, 3, -10, -15,
              2, 6, -10, 6, 3, -1,  5, -5, -8, 11, 7, -9, -5,  -6, -2,
              7, 8, 11, 8,  6, -1, -6,  9, 8, 6, -3, 4,  -8, 3, -4, 1,
